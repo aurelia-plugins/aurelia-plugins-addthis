c4002d444547a8a51a758a86b1bae891c03d1340
Bart Lenaerts <bart.lenaerts@philips.com>
2017-06-29 11:41:32 +0200

    Typescript definitions

---
+++ dist/amd/aurelia-plugins-addthis-element.d.ts
@@ -0,0 +1,14 @@
+export declare class AddThis {
+    _config: any;
+    _element: any;
+    _scriptPromise: any;
+    class: any;
+    description: any;
+    language: any;
+    title: any;
+    url: any;
+    constructor(element: any, config: any);
+    bind(): void;
+    _initialize(): Promise<void>;
+    _loadApiScript(): void;
+}
